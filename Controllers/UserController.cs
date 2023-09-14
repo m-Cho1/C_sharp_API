90cb783b80a35f5d4c32022e62e035f292cbb061
m-Cho1 <mturtle051@gmail.com>
2023-09-13 21:01:37 -0700

    worked on PUT and POST feature of api

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -62,4 +62,53 @@ public class UserController : ControllerBase
 
     }
 
+    [HttpPut("EditUser")]
+    public IActionResult EditUser(User user)
+    {
+        string sql = @"
+            UPDATE TutorialAppSchema.Users
+            SET [FirstName] =  '" + user.FirstName +
+                "', [LastName] =  '" + user.LastName +
+                "', [Email] =  '" + user.Email +
+                "', [Gender] =  '" + user.Gender +
+                "', [Active] = '" + user.Active +
+            "' WHERE UserId = " + user.UserId;
+
+            Console.WriteLine(sql);
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to update User");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(User user)
+    {
+        string sql = @" INSERT INTO TutorialAppSchema.Users(
+                    [FirstName],
+                    [LastName],
+                    [Email],
+                    [Gender],
+                    [Active]
+            ) VALUES (" +
+                "'" + user.FirstName +
+                "', '" + user.LastName +
+                "', '" + user.Email +
+                "', '" + user.Gender +
+                "', '" + user.Active +
+            "')";
+
+        Console.WriteLine(sql);
+
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+
+        throw new Exception("Failed to add User");
+    }
+
 }
